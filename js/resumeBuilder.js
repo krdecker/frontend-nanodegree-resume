57d779bcae7ac3df9a03d24afab5caa7b5982b21
krdecker <krd.345@gmail.com>
2015-01-08 14:39:51 -0800

    sub Name & Role

---
+++ js/resumeBuilder.js
@@ -0,0 +1,5 @@
+var formattedName = HTMLheaderName.replace("%data%", "KR Decker");
+$("#header").append(formattedName);
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+$("#header").append(formattedRole);
+

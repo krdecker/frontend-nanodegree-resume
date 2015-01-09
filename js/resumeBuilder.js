c87bb77f58916e8fc95b755c810e8ce2bf93071c
krdecker <krd.345@gmail.com>
2015-01-08 21:01:10 -0800

    add nested contacts object & skills array

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,18 +2,20 @@
 var bio = {
     "name" : "KR Decker",
     "role" : "Web Developer",
-    "email" : "krd.345@gmail.com",
-    "github" : "https://github.com/krdecker",
-    "pictureURL" : "images/krd.png",
+    "contacts" : {
+        "email" : "krd.345@gmail.com",
+        "github" : "https://github.com/krdecker"
+    },
     "welcomeMsg" : ". . . inquisitive mind; . . . relentless enthusiasm; . . . poetic nature.",
-    "skills" : " Python; JavaScript; jQuery; HTML/CSS."
+    "skills" : [ "Python" , "JavaScript" , "jQuery" , "HTML/CSS" ],
+    "bioPic" : "images/krd.png"
 };
 
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedEmail = HTMLemail.replace("%data%", bio.email);
-var formattedGitHub = HTMLgithub.replace("%data%", bio.github);
-var formattedPictureURL = HTMLbioPic.replace("%data%", bio.pictureURL);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+var formattedGitHub = HTMLgithub.replace("%data%", bio.contacts.github);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
 var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMsg);
 var formattedSkills = HTMLskills.replace("%data%", bio.skills);
 
@@ -23,7 +25,7 @@ $("#header").prepend(formattedRole); // LIFO
 $("#header").prepend(formattedName);
 $("#topContacts").append(formattedEmail);
 $("#topContacts").append(formattedGitHub);
-$("#topContacts").append(formattedPictureURL);
+$("#topContacts").append(formattedBioPic);
 $("#topContacts").append(formattedWelcomeMsg);
 
 $("#header").append(HTMLskillsStart);

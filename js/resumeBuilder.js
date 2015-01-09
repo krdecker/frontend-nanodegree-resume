dab5133c5458b512f12de348916d038946ffec18
krdecker <krd.345@gmail.com>
2015-01-09 15:47:17 -0800

    add resume objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,16 +1,17 @@
 
 var bio = {
-    "name" : "KR Decker",
-    "role" : "Web Developer",
+    "name" : "KR Decker" ,
+    "role" : "Web Developer" ,
     "contacts" : {
-        "email" : "krd.345@gmail.com",
+        "email" : "krd.345@gmail.com" ,
         "github" : "https://github.com/krdecker"
-    },
-    "welcomeMsg" : ". . . inquisitive mind; . . . relentless enthusiasm; . . . poetic nature.",
-    "skills" : [ "Python" , "JavaScript" , "jQuery" , "HTML/CSS" ],
+    } ,
+    "welcomeMsg" : ". . . inquisitive mind; . . . relentless enthusiasm; . . . poetic nature." ,
+    "skills" : [ "  Python" , "  JavaScript" , "  jQuery" , "  HTML/CSS" ] ,
     "bioPic" : "images/krd.png"
-};
+} ;
 
+/*
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
@@ -30,3 +31,148 @@ $("#topContacts").append(formattedWelcomeMsg);
 
 $("#header").append(HTMLskillsStart);
 $("#skills").append(formattedSkills);
+*/
+
+// ============================
+
+var work =
+    [
+        {
+            "organisation" : "International Alliance of Theatrical & Stage Employees" ,
+            "position" : "Motion Picture Lighting Technician" ,
+            "location" : "Vancouver" ,
+            "dates" : { "begin" : 1992 , "end" : 2015 } ,
+            "company" : "Decker Illumination Ltd." ,
+            "description" : "As member of a team, install and operate set lighting for major studio and location shooting.
+                        See resume at [link] (http://www.imdb.com/name/nm0213858/) imdb.com" ,
+            "url" : "http://www.iatse.com"
+        } ,
+
+        {
+            "organisation" : "Vancouver Film School"
+            "position" : "Head Instructor: Film & Video Production" ,
+            "location" : "Vancouver" ,
+            "dates" : { "begin" : 1989 , "end" : 1992 } ,
+            "company" : "Applied Video Display" ,
+            "duties" : "Teach courses and conduct workshops on methods and means of film & video production.
+                            Organise programme, including booking instructors and courses." ,
+            "url" : "http://vfs.edu"
+        } ,
+
+        {
+            "organisation" : "Concordia University, Faculty of Fine Arts"
+            "position" : "Assistant Professor" ,
+            "location" : "Montreal" ,
+            "dates" : { "begin" : 1982 , "end" : 1988 } ,
+            "company" : "Decker Illumination Ltd." ,
+            "duties" : "Teach courses on film/video art and writing for same." ,
+            "url" : "https://www.concordia.ca/"
+        } ,
+
+        {
+            "organisation" : "Logo Computer Systems Inc." ,
+            "location" : "Montreal" ,
+            "position" : "Logo Developer" ,
+            "dates" : { "begin" : 1983 , "end" : 1984 } ,
+            "company" : "Nerf Net Ltd." ,
+            "duties" : "Develop application in Logo to teach children use of keyboard." ,
+            "url" : "http://www.imdb.com/name/nm0213858/"
+        } ,
+
+    ] ;
+
+
+
+
+
+// ===================================
+
+var education = {
+
+    "schools" : [
+
+        {
+            "name" : "Concordia University",
+            "location" : "Montreal",
+            "dates" : "" ,
+            "degree" : "Master of Arts"
+            "major" : "Creative Writing"
+        },
+
+        {
+            "name" : "Concordia University",
+            "location" : "Montreal",
+            "dates" : "" ,
+            "major" : "Film Aesthetics & Production" ,
+            "degree" : "Bachelor of Fine Arts"
+        } ,
+
+        {
+            "name" : "University of Saskatchewan",
+            "location" : "Saskatoon",
+            "dates" : "" ,
+            "major" : "Chemistry & English Lit."
+            "degree" : "Bachelor of Liberal Arts"
+        }
+
+    ] ,
+
+    "on-line classes" : [
+        {
+            "title" : "Front End Developer nano-degree programme"
+            "school" : "Udacity",
+            "dates" : "Winter 2014-15" ,
+            "url" : "https://www.udacity.com/nanodegrees"
+        } ,
+
+        {
+            "title" : "Google Python Workshop"
+            "school" : "Google",
+            "dates" : "Summer 2012" ,
+            "url" : "https://developers.google.com/edu/python/"
+        } ,
+
+        {
+            "title" : "Web App Development"
+            "school" : "Udacity",
+            "dates" : "Spring 2012" ,
+            "url" : "https://www.udacity.com/course/cs253"
+        }
+
+    ]
+
+};
+
+//===================
+
+var projects = {
+
+    {
+        "title" : "" ,
+        "dates" : "" ,
+        "description" : "" ,
+        "image" : ""
+    } ,
+
+    {
+        "title" : "" ,
+        "dates" : "" ,
+        "description" : "" ,
+        "image" : ""
+    } ,
+
+    {
+        "title" : "" ,
+        "dates" : "" ,
+        "description" : "" ,
+        "image" : ""
+    }
+
+}
+
+
+
+
+
+
+
